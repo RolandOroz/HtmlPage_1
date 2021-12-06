1905126e7db1d7063bf15d763eabf29d0424976c
RolandD13 <rolandoroz@gmail.com>
2021-12-06 13:31:21 +0100

    created inline js functions to change p-tags color on page1 & page2. moved css and js files to relative folders

---
+++ js/page1Scrp.js
@@ -0,0 +1,12 @@
+function colorChangeOnOver() {
+  let p = document.getElementById("pOnPage1");
+  //change color on mouseover event
+    p.style.color = "#FFCC00";
+}
+
+function colorChangeOnLeave() {
+  let p = document.getElementById("pOnPage1");
+  //change color on mouseover event
+    p.style.color = "#000";
+}
+
