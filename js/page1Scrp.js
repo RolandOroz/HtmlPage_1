439515d31b71fa205b66ce205fc358cfaa9595b9
RolandD13 <rolandoroz@gmail.com>
2021-12-08 14:04:53 +0100

    made some swap functions

--- js/page1Scrp.js
+++ js/page1Scrp.js
@@ -3,6 +3,11 @@ function colorChange(el,hexColorOnlyNumber) {
       el.style.color = "#" + hexColorOnlyNumber;
 }
 
+//change by css class
+function colorChangeByClass(id, classN) {
+  document.getElementById(id).className = classN;
+}
+
 //changing tag atributes
 function changeAttrib(id, atrib, className ) {
     console.log(arguments);
@@ -10,15 +15,48 @@ function changeAttrib(id, atrib, className ) {
 }
 
 //changing tag positions
-function swapPositions(id) {
-  //document.getElementById(id).
+function showContent(tagN) {
+  console.log(arguments);
+  tagN = document.getElementsByTagName(tagN);
+    for(let i = 0; i < tagN.length; i++) {
+    console.log(tagN[i].innerText);
+    console.log(arguments);
+  }
+}
 
-
+function swapContent(id1, id2) {
+  //const link = document.getElementById(id);
+  /*console.log(link.childElementCount);
+  let tagFirst = link.getElementsByTagName('p')[0].innerText);
+  let firstC = link.firstElementChild.innerHTML;
+  let lastC = link.lastElementChild.innerHTML;
+*/
+/*  console.log(firstC);
+  console.log(lastC);*/
+  let tagFirst = document.getElementById(id1).lastChild;
+  document.getElementById(id2).appendChild(tagFirst);
+  console.log(tagFirst);
+  let tagLast = document.getElementById(id2).firstChild;
+  document.getElementById(id1).appendChild(tagLast);
 }
 
 //resets atribute
 function resetT(el, atr) {
   //document.getElementById(id).removeAttribute(atr);
   el.removeAttribute(atr); //nared s class
+}
+
+function resetClass(elClass,atr) {
+  //console.log(arguments);
+  elClass.removeAttribute(atr);
+}
+
+function swapTag(swap1,swap2) {
+  let tag1 = document.getElementById(swap1);
+  let tag2 = document.getElementById(swap2);
+
+  let temp = tag1.innerHTML;
+  tag1.innerHTML = tag2.innerHTML;
+  tag2.innerHTML = temp;
 }
 
