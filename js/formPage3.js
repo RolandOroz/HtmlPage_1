0702b7d0e26bea0d2e5c3c6a6d863830b7ac9294
RolandD13 <rolandoroz@gmail.com>
2021-12-13 13:42:43 +0100

    added class, names, etc. to form on page 3. function for JSON format converter is in progress.

--- js/formPage3.js
+++ js/formPage3.js
@@ -39,16 +39,89 @@ function myFunction(id) {
   console.log(toJSON);
 }
 
-function getFieldName() {
+function getFieldName(selectors) {
 
   /*let selectFieldName = document.querySelectorAll('')*/
-  let fieldName = document.querySelectorAll('.formUsr');
+  let fieldName = document.querySelectorAll(selectors);
   for (i =0; i < fieldName.length; i++) {
     let name = fieldName[i].name;
+
     let value = fieldName[i].value;
     let selectName= document.getElementById('ageGroup').options;
 
-    console.log(JSON.stringify(name) + ":" + "\"" +(value)+ "\"");
+
+    console.log(JSON.stringify(name) + ":" + "\"" +(value)+ "\"" );
 
   }};
+
+function fieldN(id) {
+  let inputF = document.getElementById(id);
+  inputF = inputF.getElementsByTagName('input');
+
+  let selectOpt = document.getElementById('ageGroup');
+  selectOpt = selectOpt.getElementsByTagName('option');
+
+  let radio = document.getElementsByName('sex');
+  let names;
+
+  for (let i = 0; i < inputF.length; i++) {
+    let namesInp = inputF[i].name;
+    let val = inputF[i].value;
+
+    console.log(JSON.stringify(namesInp) + (val));
+  }
+  for(let j = 0; j < selectOpt.length; j++) {
+    let namesOpt = selectOpt[j].value;
+    console.log(JSON.stringify(namesOpt));
+  }
+}
+
+function radioValue(name) {
+  let radioSex = document.forms[name]['sex'].value;
+  let optionAgeGroup = document.forms[name]['ageGroup'].value;
+
+  let form = document.forms[name];
+
+
+  let i;
+  let names;
+  let values;
+  let dataS = new data();
+  for(i = 0; i < form.elements.length; i++) {
+   names = names + form.elements[i].name;
+   values = values + form.elements[i].value;
+  }
+
+
+  console.log(form.length);
+  console.log(names);
+  console.log(values);
+  console.log(radioSex);
+  console.log(optionAgeGroup);
+
+}
+
+function Data(nameF) {
+ //this.radioF = document.forms[nameF]['sex'].value;
+ //this.ageGroupF = document.forms[nameF]['ageGroup'].value;
+  /*console.log(ageGroupF);
+ console.log(radioF);*/
+
+  this.form = document.forms[nameF];
+  let i;
+  let us1 = form.name;
+  let values;
+
+  for(i = 0; i < form.elements.length; i++) {
+    us1 = us1 + form.elements[i].name;
+    values = us1 + form.elements[i].value;
+  }
+  let us = Object.keys(us1);
+
+
+
+ console.log(form.elements.adress.value);
+ console.log(us1);
+}
+
 
