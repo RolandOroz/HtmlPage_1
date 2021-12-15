2312b1747e8d078f08afdedd4b6cb351edd09ef9
RolandOroz <rolandoroz@gmail.com>
2021-12-15 21:56:12 +0100

    making two slice arrays from ASCII dec values and then join into one array for only (A-Z & a-z char in dec)

--- Vaje/Nizi.js
+++ Vaje/Nizi.js
@@ -1,31 +1,25 @@
-let arrayRandN = [];  //1.naloga
+const decASCIITable = []; //0-127 ASCII dec values  //1.naloga
+
 let x;
-const decASCIITable = []; //0-127
+let counterASCII = 127;
 
-
-
-
-let counter;
-
-
-//ASCII table in DEC
-for (let i = 0; i <= 127; i++) {
+//for loop to populate array indexes with num 0-127
+for (let i = 0; i <= counterASCII; i++) {
     decASCIITable[i] = i;
     // console.log(decAtoZUpper[i]);
 }
-
-
-function sliceAToZ(toArray, start, end) {
-    toArray = decASCIITable.slice(start, end);
-    return toArray;
+//function
+function sliceASCII(fromArray, start, end) {
+    fromArray = decASCIITable.slice(start, end);
+    return fromArray;
 }
-let decAtoZUpper = sliceAToZ(decASCIITable, 65, 91);
-let decAtoZLower = sliceAToZ(decASCIITable, 97, 124);
+let decAtoZUpper = sliceASCII(decASCIITable, 65, 91);   //97-122 ASCII
+let decAtoZLower = sliceASCII(decASCIITable, 97, 124);  //65-90  ASCII
 
 //****************************************************
 // console.log(RandomCreator(arrayN, 26, 1, 102));
-console.log("\na-z slice values= " + sliceAToZ(decAtoZLower, 97, 124));  //97-122 ASCII
-console.log("A-Z slice values= " + sliceAToZ(decAtoZUpper, 65, 91));     //65-90  ASCII
+console.log("\na-z slice values= " + sliceASCII(decAtoZLower, 97, 124));
+console.log("A-Z slice values= " + sliceASCII(decAtoZUpper, 65, 91));
 
 console.log("\nASCII Table dec array --> " + decASCIITable);
 console.log("\nA-Z array--> " + decAtoZUpper)
