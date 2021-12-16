e0947dbd9949772e5b77498b47131d88f9de26ed
RolandD13 <rolandoroz@gmail.com>
2021-12-16 10:21:27 +0100

    in Nizi.js randomly populated randomValues array from ASCII array.

--- Vaje/Nizi.js
+++ Vaje/Nizi.js
@@ -1,16 +1,17 @@
-const randomValues = [];   //1.naloga
+let randomValues = [];   //1.naloga
 const decASCIITable = []; //0-127 ASCII dec values
 let counterASCII = 127;
+let random;
+let randomD;
+let limit = 25; //random counter limit
 
 //for loop to populate array indexes with num 0-127
 for (let i = 0; i <= counterASCII; i++) {
-    decASCIITable[i] = i;
-    // console.log(decAtoZUpper[i]);
+  decASCIITable[i] = i;
 }
 //FUNCTION for slicing array
 function sliceASCII(fromArray, start, end) {
-    fromArray = decASCIITable.slice(start, end);
-    return fromArray;
+  return fromArray.slice(start, end);
 }
 //made two arrays one for A-Z & other for a-z ASCII dec value
 let decAtoZUpper = sliceASCII(decASCIITable, 65, 91);   //97-122 ASCII
@@ -19,8 +20,21 @@ let decAtoZLower = sliceASCII(decASCIITable, 97, 124);  //65-90  ASCII
 //joining two arrays in to one array of ASCII dec values from A-Z to a-z
 const combinedAaZzArray = decAtoZUpper.concat(decAtoZLower);
 
+//RANDOM FUNCTION EXMPL with Math.random
+function randomCreator(array) {
+  random = array[Math.floor(Math.random() * limit)]
+}
+//for loop to populat random items in ahttps://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operatorrray
+for (let i = 0; i < limit; i++) {
+  randomValues[i] = combinedAaZzArray[Math.floor(Math.random() * combinedAaZzArray.length)] ;
+  // randomValues.push(combinedAaZzArray[Math.floor(Math.random() * combinedAaZzArray.length)]); isti kurac
+}
+//TIP**********
+//start learning conditional (ternary) operator
+// (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator)
+
 //****************** Code checking ***************************************************
-// console.log(RandomCreator(arrayN, 26, 1, 102));
+// console.log(RandomCreator(arrayN, 26,https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator 1, 102));
 console.log("\na-z slice values= " + sliceASCII(decAtoZLower, 97, 124));
 console.log("A-Z slice values= " + sliceASCII(decAtoZUpper, 65, 91));
 
@@ -28,16 +42,20 @@ console.log("\nASCII Table dec array --> " + decASCIITable);
 console.log("\nA-Z array--> " + decAtoZUpper);
 console.log("a-z array--> " + decAtoZLower);
 console.log("\nCombined arrray --> " + combinedAaZzArray);
+//console.log(randomCreator(combinedAaZzArray));
+//console.log(random)
+console.log("new array of random numbers --> " + randomValues)
+
+
 
 //******************* ideas to implement for later ************************************
-/* //RANDOM FUNCTION EXMPL
-function RandomCreator( limit,min, max) {
-    for (let i = 0; i < limit; i++) {
-        random = Math.floor(Math.random() * (max)+min);
-        arrayRandN[i] = i + random;
-        console.log(arrayRandN[i] + " " + i);
-    }
-}
+
+//RANDOM FUNCTION EXMPL with Date.now
+function randomArr(array) {
+
+  randomD = array[Date.now() % array.length];
+}// TODO --> populate randomValues Array with (for) loop
+
 /* primer padajočega izpisa v C#
 public static void Izpis(int[] tab)
 {
@@ -59,6 +77,5 @@ for (int i = 0; i < counter; i++)
 console.log(RandomCreator( 26, 1, 100));*/
 
 //********************** side notes *******************************
-// TODO --> random value selector from combined array with pick limiter
-// TODO --> populate randomValues Array with (for) loop
+
 // TODO --> converter from dec to char
