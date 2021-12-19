702aca826d84dd5b7e143021990fde1fa8eb7a39
RolandOroz <rolandoroz@gmail.com>
2021-12-19 10:27:25 +0100

    learning class and getters setters

--- Vaje/TipiObjekti.js
+++ Vaje/TipiObjekti.js
@@ -1,5 +1,30 @@
-//naredi class
-let circleRadius;
-const PI = Math.PI;
-let areaOfCircle = PI * (circleRadius * circleRadius);
-let circumferenceOfCircle = 2* PI * circleRadius;
+// class
+class Circle {
+    constructor(_radius) {
+        this.radius = _radius;
+    }
+
+    //method AREA
+    circArea() {
+        return Math.PI*(this.radius * this.radius);
+    }
+    //method DIAMETER
+    circDiameter() {
+        return 2 * Math.PI * this.radius;
+    }
+    //method DESCRIPTION of class
+    printDescription() {
+        console.log('krog 1: ploščina ${radius} obseg: ${this.circDiameter}');
+    }
+
+}
+// TODO making a function for random radius values
+function randRadius() {
+
+}
+
+let Circle1 = new Circle(2);
+console.log(Circle1.circArea() + " " + Circle1.circDiameter());
+console.log(Circle1);
+console.log(Circle1 instanceof Circle);
+Circle1.printDescription();
